7f6c5af921c9ddd7d0e0085748c23aeac7037343
AndyArch11 <andy.arch11@gmail.com>
2026-03-17 08:06:45 +0000

    Foundry network access issue

--- infra/terraform/modules/foundry/main.tf
+++ infra/terraform/modules/foundry/main.tf
@@ -4,10 +4,15 @@ resource "azurerm_cognitive_account" "foundry" {
   resource_group_name           = var.resource_group_name
   kind                          = "AIServices"
   sku_name                      = "S0"
-  public_network_access_enabled = false
+  public_network_access_enabled = true
   custom_subdomain_name         = "foundry-${var.suffix}"
   project_management_enabled    = true
   tags                          = var.tags
+
+  network_acls {
+    default_action = "Deny"
+    bypass         = "AzureServices"
+  }
 
   identity {
     type = "SystemAssigned"
